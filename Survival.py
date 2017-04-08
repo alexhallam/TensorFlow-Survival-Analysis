56bfd82f8b08b5a0a1e396bb73f419b3d54c333a
alexhallam <alexhallam6.28@gmail.com>
2017-04-08 16:49:19 +0000

    try true risk

--- Survival.py
+++ Survival.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 # Read data
-df = pd.read_csv("simulated_dat.csv")
+#df = pd.read_csv("simulated_dat.csv")
+df = pd.read_csv("true.csv")
 
 # 1. Identify model and cost
 # Linear Regression Model y_hat = Wx+b
@@ -23,15 +24,16 @@ df = pd.read_csv("simulated_dat.csv")
 x = tf.placeholder(tf.float32, [None, 1])
 y = tf.placeholder(tf.float32, [None, 1])
 e = tf.placeholder(tf.float32, [None, 1])
+risk_true = tf.placeholder(tf.float32, [None, 1])
 #variables - initialize to vector of zeros
 W = tf.Variable(tf.zeros([1,1]))
 #b = tf.Variable(tf.zeros([1]))
 
 #model and cost
 risk = tf.matmul(x,W)
-cost = -tf.reduce_sum((risk - tf.log(tf.cumsum(tf.exp(risk))))*e)
+cost = -tf.reduce_sum((risk - tf.log(tf.cumsum(tf.exp(risk_true))))*e)
 #Gradient Descent
-train_step = tf.train.GradientDescentOptimizer(0.0001).minimize(cost)
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
 
 #TensorFlow quarks
 init = tf.global_variables_initializer()
@@ -41,18 +43,19 @@ sess = tf.Session()
 sess.run(init)
 
 #Generate data
-for i in range(500):
+for i in range(1000):
     #features = np.array([[i]])
-    features = np.array(df[['x']])
+    features = np.array(df[['x2']])
     #target = np.array([[i*4]])
-    target = np.array(df[['t']])
-    censored = np.array(df[['e']])
+    target = np.array(df[['time']])
+    censored = np.array(df[['is_censored']])
+    risk_t = np.array(df[['risk']])
     #feed in data from placeholers
-    feed = { x: features, y: target, e: censored }
+    feed = { x: features, y: target, e: censored, risk_true: risk_t}
     sess.run(train_step, feed_dict=feed)
     if i % 50 == 0:
         print("After %d iteration:" % i)
-        print("W: %f" % sess.run(W))
+        print("W h(x): %f" % sess.run(W))
         print("cost: %f" % sess.run(cost, feed_dict=feed))
 
 
